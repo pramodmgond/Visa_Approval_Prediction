3134e730a256b2f87033009a27775efb480d85f6
pramod <pramodgond0075@gmail.com>
2024-06-22 12:57:11 +0530

    data drift

--- demo.py
+++ demo.py
@@ -1,6 +1,20 @@
 # File: main.py
-from US_Visa.logger import logging
+# from US_Visa.logger import logging
 
-logging.info("Logger initialized successfully")
+# #logging.info("Logger initialized successfully")
+# from US_Visa.exception import USvisaException
+# import sys
+
+# try:
+#     a = 1/'10'
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e,sys) from e
+from US_Visa.pipline.training_pipeline import TrainPipeline
+
+
+pipline  = TrainPipeline()
+pipline.run_pipeline()
+
 
 
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 from US_Visa.components.data_ingestion import main
+from US_Visa.constants import *
 
-main()
+print(DATABASE_NAME)
+
+
+
+#main()
+
+
